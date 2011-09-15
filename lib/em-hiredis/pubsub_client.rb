fe3244ad924a3666ce629726398b995d80b678a3
Martyn Loughran <me@mloughran.com>
2011-09-15 21:35:28 +0100

    Emit extra pubsub events: subscribe unsubscribe psubscribe punsubscribe
    * It's still also possible to use the deferrable from subscribe, unsubscribe etc. which succeeds when the subscription succeeds
    * Internally allows us to optimise resubscription on reconnect to use a single redis call

--- lib/em-hiredis/pubsub_client.rb
+++ lib/em-hiredis/pubsub_client.rb
@@ -1,9 +1,10 @@
 module EventMachine::Hiredis
   class PubsubClient < Client
-    PUBSUB_MESSAGES = %w{message pmessage}.freeze
+    PUBSUB_MESSAGES = %w{message pmessage subscribe unsubscribe psubscribe punsubscribe}.freeze
 
     def initialize(host, port, password = nil, db = nil)
       @subs, @psubs = [], []
+      @pubsub_defs = Hash.new { |h,k| h[k] = [] }
       super
     end
 
@@ -11,10 +12,9 @@ module EventMachine::Hiredis
       @sub_callbacks = Hash.new { |h, k| h[k] = [] }
       
       # Resubsubscribe to channels on reconnect
-      # TODO: Send all subscriptions using one call to redis
       on(:reconnected) {
-        @subs.each { |s| method_missing(:subscribe, s) }
-        @psubs.each { |s| method_missing(:psubscribe, s) }
+        raw_send_command(:subscribe, *@subs) if @subs.any?
+        raw_send_command(:psubscribe, *@psubs) if @psubs.any?
       }
       
       super
@@ -32,7 +32,8 @@ module EventMachine::Hiredis
         @sub_callbacks[channel] << cb
       end
       @subs << channel
-      method_missing(:subscribe, channel)
+      raw_send_command(:subscribe, channel)
+      return pubsub_deferrable(channel)
     end
     
     # Unsubscribe all callbacks for a given channel
@@ -42,7 +43,8 @@ module EventMachine::Hiredis
     def unsubscribe(channel)
       @sub_callbacks.delete(channel)
       @subs.delete(channel)
-      method_missing(:unsubscribe, channel)
+      raw_send_command(:unsubscribe, channel)
+      return pubsub_deferrable(channel)
     end
 
     # Unsubscribe a given callback from a channel. Will unsubscribe from redis
@@ -64,16 +66,38 @@ module EventMachine::Hiredis
 
     def psubscribe(channel)
       @psubs << channel
-      method_missing(:punsubscribe, channel)
+      raw_send_command(:punsubscribe, channel)
+      return pubsub_deferrable(channel)
     end
 
     def punsubscribe(channel)
       @psubs.delete(channel)
-      method_missing(:punsubscribe, channel)
+      raw_send_command(:punsubscribe, channel)
+      return pubsub_deferrable(channel)
     end
     
     private
     
+    # Send a command to redis without adding a deferrable for it. This is
+    # useful for commands for which replies work or need to be treated
+    # differently
+    def raw_send_command(sym, *args)
+      if @connected
+        @connection.send_command(sym, *args)
+      else
+        callback do
+          @connection.send_command(sym, *args)
+        end
+      end
+      return nil
+    end
+
+    def pubsub_deferrable(channel)
+      df = EM::DefaultDeferrable.new
+      @pubsub_defs[channel].push(df)
+      df
+    end
+
     def handle_reply(reply)
       if reply && PUBSUB_MESSAGES.include?(reply[0]) # reply can be nil
         kind, subscription, d1, d2 = *reply
@@ -88,6 +112,18 @@ module EventMachine::Hiredis
         when :pmessage
           # Arguments are original pattern, channel, message payload
           emit(:pmessage, subscription, d1, d2)
+        else
+          if @pubsub_defs[subscription].any?
+            df = @pubsub_defs[subscription].shift
+            df.succeed(reply)
+            # Cleanup empty arrays
+            if @pubsub_defs[subscription].empty?
+              @pubsub_defs.delete(subscription)
+            end
+          end
+
+          # Also emit the event, as an alternative to using the deferrables
+          emit(kind.to_sym, subscription, d1)
         end
       else
         super
