50cf86afdf985c3570ef9b54916e19f1fe2e68ef
Martyn Loughran <me@mloughran.com>
2011-12-09 18:34:41 +0000

    Move pubsub methods

--- lib/em-hiredis/client.rb
+++ lib/em-hiredis/client.rb
@@ -20,5 +20,30 @@ module EventMachine::Hiredis
       end
       method_missing(:info, &hash_processor)
     end
+
+    # Gives access to a richer interface for pubsub subscriptions on a
+    # separate redis connection
+    #
+    def pubsub
+      @pubsub ||= begin
+        PubsubClient.new(@host, @port, @password, @db).connect
+      end
+    end
+
+    def subscribe(*channels)
+      raise "Use pubsub client"
+    end
+
+    def unsubscribe(*channels)
+      raise "Use pubsub client"
+    end
+
+    def psubscribe(channel)
+      raise "Use pubsub client"
+    end
+
+    def punsubscribe(channel)
+      raise "Use pubsub client"
+    end
   end
 end
