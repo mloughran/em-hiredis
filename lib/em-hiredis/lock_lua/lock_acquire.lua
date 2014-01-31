86ced27953164327676fa69a5319cf27561b2fdf
Michael Pye <mail@mdpye.co.uk>
2014-01-31 13:59:15 +0000

    Use more backward compatible formulation of SET EX

--- lib/em-hiredis/lock_lua/lock_acquire.lua
+++ lib/em-hiredis/lock_lua/lock_acquire.lua
@@ -10,7 +10,7 @@ local value = redis.call('get', key)
 
 if value == token or not value then
     -- Great, either we hold the lock or it's free for us to take
-    return redis.call('set', key, token, 'ex', timeout)
+    return redis.call('setex', key, timeout, token)
 else
     -- Someone else has it
     return false
